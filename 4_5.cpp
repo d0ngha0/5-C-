ef8b86d7cb596263971af824348310c7dde5b345
d0ngha0 <31814995+d0ngha0@users.noreply.github.com>
2018-08-31 08:55:51 +0800

    Add files via upload

--- 4_5.cpp
+++ 4_5.cpp
@@ -4,25 +4,25 @@ using namespace std;
 
 class express {
 	public:
-		int ex_pow(int a, int n);
+		long long ex_pow(long long a, long long n);
 }; 
 
-int express :: ex_pow (int a, int n) {
-	
+long long express :: ex_pow (long long a, long long n) { //为什么只改递归函数的返回值不可以， 
+														// 而要将所有有关变量的类型都改成 long long 
 	if (n == 0) return 1;
 	
 	if (n % 2 == 1) {
-		return ex_pow(a,(n - 1) / 2) * ex_pow(a,(n - 1) / 2) * a;		
+		return (ex_pow(a,(n - 1) / 2) * ex_pow(a,(n - 1) / 2) * a) % 10007;		
 	}
 	
 	else {
-		return ex_pow(a,(n) / 2) * ex_pow(a,(n) / 2);	
+		return (ex_pow(a,(n) / 2) * ex_pow(a,(n) / 2) ) % 10007;	
 	}
 }
 
 int main() {
 	express s;
-	int a,n;
+	long long a,n;
 	cin >> a >> n;
 	cout << s.ex_pow(a,n) << endl;
 }
